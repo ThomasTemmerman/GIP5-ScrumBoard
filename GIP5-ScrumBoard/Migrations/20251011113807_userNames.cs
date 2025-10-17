d6569e512ccb756d6f683ce72d38e3814f4fda19
Thomas Temmerman <r0992650@ucll.be>
2025-10-17 13:42:31 +0200

    Added FontAwesome Icons + better navigations between views

---
+++ GIP5-ScrumBoard/Migrations/20251011113807_userNames.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GIP5_ScrumBoard.Migrations
+{
+    /// <inheritdoc />
+    public partial class userNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
